0400101694b77a39b4bc14cd58db465a0ca89c57
tim <jan-brenner@gmx.de>
2017-03-22 21:26:34 +0100

    Initial commit.

---
+++ AES-TextCryptor/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("AES-TextCryptor")>
+<Assembly: AssemblyDescription("AES-TextCryptor")>
+<Assembly: AssemblyCompany("Hämmer Electronics")>
+<Assembly: AssemblyProduct("AES-TextCryptor")>
+<Assembly: AssemblyCopyright("Copyright © AES-TextCryptor")>
+<Assembly: AssemblyTrademark("AES-TextCryptor")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("69d3a5a6-843d-4718-a11f-63e1f34869ad")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ README.md
@@ -0,0 +1,21 @@
+AES-TextCryptor
+===============
+
+AES-TextCryptor is an executable to encrypt and decrypt strings.
+The executable was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/g6cm8rov8b4ea07q?svg=true)](https://ci.appveyor.com/project/SeppPenner/aes-textcryptor)
+
+## Screenshot from the executable english:
+![Screenshot from the executable english](https://github.com/SeppPenner/AES-TextCryptor/blob/master/Screenshot_EN.PNG "Screenshot from the executable english")
+
+## Screenshot from the executable german:
+![Screenshot from the executable german](https://github.com/SeppPenner/AES-TextCryptor/blob/master/Screenshot_DE.PNG "Screenshot from the executable german")
+
+## Changing the language permanentely:
+Edit the config.ini file in the application directory: EN and DE are valid inputs.
+
+Change history
+--------------
+
+* **Version 1.0.0.0 (2017-03-22)** : 1.0 release.
